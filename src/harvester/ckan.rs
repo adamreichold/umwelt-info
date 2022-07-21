3f3e87b861ae5b61643d39a599c8d67854b4227f
Adam Reichold <adam.reichold@t-online.de>
2022-07-21 19:27:32 +0200

    Remove the initial CKAN harvester as it is less efficient with no known benefits.

--- src/harvester/ckan.rs
+++ src/harvester/ckan.rs
@@ -7,96 +7,17 @@ use serde::Deserialize;
 use crate::{dataset::Dataset, harvester::Source};
 
 pub async fn harvest(dir: &Dir, client: &Client, source: &Source) -> Result<()> {
-    let url = source.url.join("api/3/action/package_list")?;
+    let rows = source.batch_size;
 
-    let package_list = client
-        .get(url)
-        .send()
-        .await?
-        .error_for_status()?
-        .json::<PackageList>()
-        .await?;
-
-    ensure!(
-        package_list.success,
-        "Failed to retrieve package list: {}",
-        package_list
-            .error
-            .as_ref()
-            .map_or("Malformed response", |err| &err.message)
-    );
-
-    let count = package_list.result.len();
-    tracing::info!("Harvesting {} datasets", count);
-
-    let errors = iter(package_list.result)
-        .map(|package_id| fetch_dataset(dir, client, source, package_id))
-        .buffer_unordered(source.concurrency.unwrap_or(1))
-        .fold(0, |mut errors, res| async move {
-            if let Err(err) = res {
-                tracing::error!("{:#}", err);
-
-                errors += 1;
-            }
-
-            errors
-        })
-        .await;
-
-    if errors != 0 {
-        tracing::error!("Failed to harvest {} out of {} datasets", errors, count);
-    }
-
-    Ok(())
-}
-
-#[tracing::instrument(skip(dir, client))]
-async fn fetch_dataset(
-    dir: &Dir,
-    client: &Client,
-    source: &Source,
-    package_id: String,
-) -> Result<()> {
-    tracing::debug!("Fetching dataset {}", package_id);
-
-    let url = source.url.join("api/3/action/package_show")?;
-
-    let package_show = client
-        .get(url)
-        .query(&[("id", &package_id)])
-        .send()
-        .await?
-        .error_for_status()?
-        .json::<PackageShow>()
-        .await?;
-
-    ensure!(
-        package_show.success,
-        "Failed to fetch package: {}",
-        package_show
-            .error
-            .as_ref()
-            .map_or("Malformed response", |err| &err.message)
-    );
-
-    write_dataset(dir, source, package_show.result).await?;
-
-    Ok(())
-}
-
-pub async fn harvest_search(dir: &Dir, client: &Client, source: &Source) -> Result<()> {
-    let concurrency = source.concurrency.unwrap_or(1);
-    let rows = source.batch_size.unwrap_or(100);
-
-    let (count, results, errors) = fetch_datasets_search(dir, client, source, 0, rows).await?;
+    let (count, results, errors) = fetch_datasets(dir, client, source, 0, rows).await?;
     tracing::info!("Harvesting {} datasets", count);
 
     let requests = (count + rows - 1) / rows;
     let start = (1..requests).map(|request| request * rows);
 
     let (results, errors) = iter(start)
-        .map(|start| fetch_datasets_search(dir, client, source, start, rows))
-        .buffer_unordered(concurrency)
+        .map(|start| fetch_datasets(dir, client, source, start, rows))
+        .buffer_unordered(source.concurrency)
         .fold(
             (results, errors),
             |(mut results, mut errors), res| async move {
@@ -125,7 +46,7 @@ pub async fn harvest_search(dir: &Dir, client: &Client, source: &Source) -> Resu
 }
 
 #[tracing::instrument(skip(dir, client))]
-async fn fetch_datasets_search(
+async fn fetch_datasets(
     dir: &Dir,
     client: &Client,
     source: &Source,
@@ -183,20 +104,6 @@ async fn write_dataset(dir: &Dir, source: &Source, package: Package) -> Result<(
     dataset.write(file).await?;
 
     Ok(())
-}
-
-#[derive(Deserialize)]
-struct PackageList {
-    success: bool,
-    error: Option<Error>,
-    result: Vec<String>,
-}
-
-#[derive(Deserialize)]
-struct PackageShow {
-    success: bool,
-    error: Option<Error>,
-    result: Package,
 }
 
 #[derive(Deserialize)]
