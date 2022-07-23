736f194c78c0c0dff499c905a3e9ede1c8f54b0c
Adam Reichold <adam.reichold@t-online.de>
2022-07-23 14:31:53 +0200

    Use LZ4 to compress datasets as it is already part of the dependency closure.

--- src/dataset.rs
+++ src/dataset.rs
@@ -3,6 +3,7 @@ use std::io::Read;
 use anyhow::Result;
 use bincode::{deserialize, serialize};
 use cap_std::fs::File;
+use lz4_flex::{compress_prepend_size, decompress_size_prepended};
 use serde::{Deserialize, Serialize};
 use tokio::{fs::File as AsyncFile, io::AsyncWriteExt};
 use url::Url;
@@ -18,15 +19,17 @@ impl Dataset {
     pub fn read(mut file: File) -> Result<Self> {
         let mut buf = Vec::new();
         file.read_to_end(&mut buf)?;
-        let val = deserialize(&buf)?;
+
+        let val = deserialize(&decompress_size_prepended(&buf)?)?;
 
         Ok(val)
     }
 
     pub async fn write(&self, file: File) -> Result<()> {
-        let mut file = AsyncFile::from_std(file.into_std());
+        let buf = compress_prepend_size(&serialize(self)?);
 
-        file.write_all(&serialize(self)?).await?;
+        let mut file = AsyncFile::from_std(file.into_std());
+        file.write_all(&buf).await?;
 
         Ok(())
     }
