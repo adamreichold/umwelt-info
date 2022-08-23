2e1946cf791e6887f225a7581097dfc9b225b69b
Adam Reichold <adam.reichold@t-online.de>
2022-08-23 16:17:22 +0200

    Strip the disambiguations from the similar terms provided by SNS.

--- src/umthes.rs
+++ src/umthes.rs
@@ -1,4 +1,8 @@
+use std::borrow::Cow;
+
 use anyhow::Result;
+use once_cell::sync::Lazy;
+use regex::Regex;
 use reqwest::Client;
 use serde::Deserialize;
 
@@ -16,10 +20,40 @@ pub async fn fetch_similar_terms(client: &Client, terms: &[String]) -> Result<Ve
         .json::<SimilarTerms>()
         .await?;
 
-    Ok(response.results)
+    let results = response
+        .results
+        .into_iter()
+        .map(strip_disambiguation)
+        .collect();
+
+    Ok(results)
 }
 
 #[derive(Deserialize)]
 struct SimilarTerms {
     results: Vec<String>,
+}
+
+fn strip_disambiguation(term: String) -> String {
+    static DISAMBIGUATION: Lazy<Regex> = Lazy::new(|| Regex::new(r#"\s+\[.+\]"#).unwrap());
+
+    match DISAMBIGUATION.replace_all(&term, "") {
+        Cow::Borrowed(_term) => term,
+        Cow::Owned(term) => term,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn keep_results_but_remove_disambiguations() {
+        assert_eq!(strip_disambiguation("Aller [Fluss]".to_owned()), "Aller");
+
+        assert_eq!(
+            strip_disambiguation("Binnenschifffahrt".to_owned()),
+            "Binnenschifffahrt"
+        );
+    }
 }
